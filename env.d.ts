5c6fe9020260938305cb74c9cac99c2c24dd67f6
witsanu <t660861@tcrbank.com>
2024-11-25 10:46:58 +0700

    set up env file

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare module '@env' {
+    export const BASE_URL_API: string;
+    export const KEY_ENCRYPT: string;
+    export const IS_ENCRYPT: string;
+    // Add other environment variables here
+}
